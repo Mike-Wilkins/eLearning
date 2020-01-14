e325282097ccd2f32e0c4f85fb8d8d979a16e470
Michael Wilkins <michaelwilkins@Michaels-MacBook-Pro.local>
2020-01-14 15:39:17 +0000

    second commit

--- credentials.php
+++ credentials.php
@@ -1,12 +1,7 @@
 <?php
 
-/*$username = 'mw123';
-$password =  'parsnip123';
-$dbname =  'kwldatabase';
-$host = 'localhost';*/
-
-$username = 'kwlusername';
-$password =  'parsnipparsnip';
-$dbname =  'kwlschema';
-$dbport = '3306';
-$host = 'kwldatabase.c6xcteqwzbvm.us-east-2.rds.amazonaws.com';
+$username = '';
+$password =  '';
+$dbname =  '';
+$dbport = '';
+$host = '';
