dc1bef415dd5db7a0b68ef069178822107fdde7d
Mike <michael-wilkins@sky.com>
2020-02-07 22:00:05 +0000

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,6 +1,6 @@
 <?php
-$username = 'kwlusername';
-$password =  'parsnipparsnip';
-$dbname =  'kwlschema';
-$dbport = '3306';
-$host = 'kwldatabase.c6xcteqwzbvm.us-east-2.rds.amazonaws.com';
+$username = '*******';
+$password =  '*********';
+$dbname =  '*********';
+$dbport = '*********';
+$host = '**********';
